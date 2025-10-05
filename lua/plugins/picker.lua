8a07330cca2be86c00ef7750fbab2110267041c4
Ollie Hunter <ollie@olliehunter.com>
2025-10-05 22:14:21 +0100

    Initial Commit
    Done some stuff felt cute - might delete later

---
+++ lua/plugins/picker.lua
@@ -0,0 +1,14 @@
+return {
+  "folke/snacks.nvim",
+  opts = {
+    picker = {
+      hidden = true,
+    },
+    explorer = {
+      git_status = true,
+      git_status_open = true,
+      git_untracked = true,
+      auto_close = true,
+    },
+  },
+}
